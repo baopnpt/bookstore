b43faa981aba562330de95a4d9365f53600070e9
Bảo Hoàng Thái <baoht@media-one.vn>
2020-11-21 09:24:19 +0700

    update

--- api/models/Books.js
+++ api/models/Books.js
@@ -6,25 +6,39 @@
  */
 
 module.exports = {
-
   attributes: {
-
-    providerId : {type : "number"},
-    publisherId : {type : "number"},
-    authorId : {type : "number"},
-    categoryId : {type : "number"},
-    name : {type : "string"},
-    price : {type : "number"},
-    salePrice : {type : "number"},
-    description : {type : "string"},
-    numberOfPage : {type : "number"},
-    images : {type : "json", defaultsTo : []},
-    isActive : {type : "number"}
+    providerId: { type: "number" },
+    publisherId: { type: "number" },
+    authorId: { type: "number" },
+    categoryId: { type: "number" },
+    name: { type: "string" },
+    price: { type: "number" },
+    percentDiscount: { type: "number" },
+    description: { type: "string" },
+    numberOfPage: { type: "number" },
+    images: { type: "json", defaultsTo: [] },
+    isActive: { type: "number" },
+    nameAlias : {type : "string"}
   },
-  getInfoById : async (id)=>{
-    let data = await Books.findOne({id});
+  afterCreate : (inst, cb)=>{
+    let nameAl = sails.helpers.common.removeAlias(inst.name);
+    Books.update(inst.id).set({nameAlias : nameAl}).then(()=>{});
+    cb();
+  },
+  getInfoById: async (id) => {
+    let data = await Books.findOne({ id });
     return data;
-  }
-
+  },
+  seacrhProductWithKeyword: async (keyword, skip, limit) => {
+    let data = await Books.find({
+      where: {
+        nameAlias: {
+          contains: keyword,
+        },
+      },
+      skip,
+      limit,
+    });
+    return data
+  },
 };
-
